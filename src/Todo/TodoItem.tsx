afc3b912fe396a9431ae1e82115bdc37985b47df
Matthew baier <m.baier161@gmail.com>
2024-06-17 23:29:10 -0400

    creating initial todo item

--- src/Todo/TodoItem.tsx
+++ src/Todo/TodoItem.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import './TodoItem.css';
+
 
 const TodoItem = () => {
     return (
-        <p>Hello World!</p>
+        <div className='container'>
+            <div className='item'>
+                <h2>Todo Item</h2>
+                <p>Task 1: <input type="text"></input></p>
+            </div>
+            <div className='item'>
+                <p>Due Date: <input type="date"></input></p>
+            </div>
+            <div className='item'>
+                <p>Complete <input type="checkbox"></input></p>
+            </div>
+            <div className='item'>
+                <p>Additional Details <input type="checkbox"></input></p>
+                <input type="text"></input>
+            </div>
+            <div className='item submit-button'>
+                <button>Create</button>
+            </div>
+        </div>
     )
 }
 
